123ac04659ae0f39370f7bfbb6bb89bacd7a6687
Laura Pedroni <laurage_@hotmail.com>
2016-11-07 11:07:40 +0100

    model created + seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.delete_all
+
+Task.new(title: "What to do today?", checked: true).save
+Task.new(title: "Unicorns!", checked: false).save
+Task.new(title: "Breakfast with Emile", checked: false).save
